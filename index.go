53583ed560bea65c1ff85006d800de09a45aed69
Hans Brigman <datyedyeguy@gmail.com>
2013-03-20 04:37:44 -0400

    Did a little code cleanup/maintenance

--- index.go
+++ index.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"labix.org/v2/mgo"
-	"labix.org/v2/mgo/bson"
 	"net/http"
 )
 
@@ -24,26 +22,19 @@ var templates = template.Must(template.ParseFiles("types.html", "main.html", "ad
 //
 ///////////////////////////////////////////////////////////////////////////////
 
+// Used to retrieve data from the salvage database
 type Salvage struct {
 	ID           string "ID"
 	SalvageCount string "SalvageCount"
 }
 
-type ItemType struct {
-	ID       string     `json:"id"`
-	Name     string     `json:"name"`
-	Subtypes []ItemType `json:"subtypes"`
-}
-
-type ItemTypeResponse struct {
-	Results []ItemType `json:"results"`
-}
-
+// Used to retrieve lists of items from GW2Spidy
 type GW2SpidyItemList struct {
 	Count int                `json:"count"`
 	Items []GW2SpidyItemData `json:"results"`
 }
 
+// Main struct containing necessary data for items
 type GW2SpidyItemData struct {
 	DataID string `bson:"data_id" json:"data_id"`
 	Name   string `bson:"name" json:"name"`
@@ -56,89 +47,52 @@ type GW2SpidyItemData struct {
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-func defaultHandler(response http.ResponseWriter, request *http.Request) {
-	session, err := mgo.Dial(DB_ADDR)
-
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-
-	c := session.DB("GuildWars2").C("salvage")
-	result := Salvage{}
-	err = c.Find(bson.M{"ID": 187}).One(&result)
-
-	if err != nil {
-		panic(err)
-	}
-
-	var buffer bytes.Buffer
-
-	tmpl, err := template.New("test").Parse("<html>ID: {{.ID}} <br />Salvage Count: {{.SalvageCount}}</html>")
-
-	if err != nil {
-		panic(err)
-	}
-
-	tmpl.Execute(&buffer, result)
-	response.Write(buffer.Bytes())
-}
-
+// Handler to show the main page
+// TODO: Find better way to execute/send html file in response
 func mainHandler(response http.ResponseWriter, requeest *http.Request) {
 	err := templates.ExecuteTemplate(response, "main.html", nil)
 	handleError(err, response, "Unable to execute template")
 }
 
-func typeHandler(response http.ResponseWriter, request *http.Request) {
-	requestURL := GW2SPIDY_URL + "types"
-	resp, err := http.Get(requestURL)
-	handleError(err, response, "Get request failed")
-	defer resp.Body.Close()
-
-	contents, err := ioutil.ReadAll(resp.Body)
-	handleError(err, response, "Unable to read body of response")
-
-	responseItemType := ItemTypeResponse{}
-	err = json.Unmarshal(contents, &responseItemType)
-	handleError(err, response, "Unable to Unmarshal contents of response")
-
-	err = templates.ExecuteTemplate(response, "types.html", responseItemType.Results)
-	handleError(err, response, "Unable to execute template")
-}
-
+// Handler to show page that allows the addition of new salvage data
 func addSalvageHandler(response http.ResponseWriter, request *http.Request) {
+	// Connect to the database
 	session, err := mgo.Dial(DB_ADDR)
-
-	if err != nil {
-		panic(err)
-	}
+	handleError(err, response, "Unable to connect to database")
 	defer session.Close()
 
-	c := session.DB("GuildWars2").C("salvageMaterials")
+	// Open up our collection
+	collection := session.DB("GuildWars2").C("salvageMaterials")
+
+	// Pull out all the data in the database
 	var result []GW2SpidyItemData
-	err = c.Find(nil).All(&result)
+	err = collection.Find(nil).All(&result)
+	handleError(err, response, "Unable to execute template")
 
-	if err != nil {
-		panic(err)
-	}
-
+	// Execute the template with the data so we can show all the material items available
 	err = templates.ExecuteTemplate(response, "addSalvage.html", map[string]interface{}{"Materials": result})
 	handleError(err, response, "Unable to execute template")
 }
 
+// Handler to show page that allows the addition of new material types
 func addSalvateTypeHandler(response http.ResponseWriter, request *http.Request) {
+	// Retrieve items under the 'Crafting Material' category
 	requestURL := GW2SPIDY_URL + "all-items/5"
 	resp, err := http.Get(requestURL)
 	handleError(err, response, "Get request failed")
 	defer resp.Body.Close()
 
+	// Read contents of request
 	contents, err := ioutil.ReadAll(resp.Body)
 	handleError(err, response, "Unable to read body of response")
 
+	// Umarshal JSON into item data objects
 	itemList := GW2SpidyItemList{}
 	err = json.Unmarshal(contents, &itemList)
 	handleError(err, response, "Unable to Unmarshal contents of response")
 
+	// TODO: Remove items already existing in salvageMaterials to avoid duplication
+	// Execute template to show checklist to add to database
 	err = templates.ExecuteTemplate(response, "addSalvageTypes.html", itemList.Items)
 	handleError(err, response, "Unable to execute template")
 }
@@ -149,35 +103,40 @@ func addSalvateTypeHandler(response http.ResponseWriter, request *http.Request) 
 //
 ///////////////////////////////////////////////////////////////////////////////
 
+// Handles the addition of new salvage data
 func libAddSalvageHandler(response http.ResponseWriter, request *http.Request) {
 
 }
 
+// Handles the addition of new salvage material data
 func libAddSalvageTypeHandler(response http.ResponseWriter, request *http.Request) {
+	// Connect to the database
 	session, err := mgo.Dial(DB_ADDR)
-
-	if err != nil {
-		panic(err)
-	}
+	handleError(err, response, "Unable to connect to database")
 	defer session.Close()
 
+	// Parse out the query parameters to make them available in the Form
 	request.ParseForm()
 
+	// Grab our collection
 	c := session.DB("GuildWars2").C("salvageMaterials")
 	var itemData GW2SpidyItemData
 
+	// Loop through all query paramters and add them to the database
 	for id := range request.Form {
 		itemData.DataID = id
 		itemData.Name = request.Form.Get(id)
-
-		fmt.Println("id: ", id, "value: ", request.Form.Get(id))
+		// TODO: Check for duplicates before inserting
 		err = c.Insert(itemData)
 
 		if err != nil {
-			panic(err)
+			fmt.Println("Unable to add id ", id, "to salvageMaterials. ", err.Error())
 		}
 	}
 
+	// Return to main page
+	// TODO: Return to a better page to show results/success
+	// TODO: Proper redirection
 	http.Redirect(response, request, "../main", http.StatusOK)
 }
 
@@ -201,9 +160,8 @@ func handleError(err error, response http.ResponseWriter, message string) {
 ///////////////////////////////////////////////////////////////////////////////
 
 func main() {
-	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/", mainHandler)
 	http.HandleFunc("/main", mainHandler)
-	http.HandleFunc("/types", typeHandler)
 	http.HandleFunc("/addSalvage", addSalvageHandler)
 	http.HandleFunc("/lib/addSalvage", libAddSalvageHandler)
 
