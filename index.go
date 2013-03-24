891ecbda12e1645d881fc70c8aa62796ebb355c8
Hans Brigman <datyedyeguy@gmail.com>
2013-03-23 20:27:03 -0400

    Added viewSalvage page to view salvage data.  Basics are working, but need to build a wrapper/new type to contain the counts of material salvaged from each item, along with salvage count.

--- index.go
+++ index.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"labix.org/v2/mgo"
@@ -18,7 +19,7 @@ const (
 	GW2SPIDY_URL           = "http://www.gw2spidy.com/api/v0.9/json/"
 )
 
-var templates = template.Must(template.ParseFiles("main.html", "addSalvage.html", "addSalvageTypes.html"))
+var templates = template.Must(template.ParseFiles("main.html", "addSalvage.html", "viewSalvage.html"))
 
 ///////////////////////////////////////////////////////////////////////////////
 //
@@ -44,11 +45,28 @@ type GW2SpidyItemList struct {
 	Items []GW2SpidyItemData `json:"results"`
 }
 
+// Used to retrieve a single item from GW2Spidy
+type GW2SpidyItemResult struct {
+	Result GW2SpidyItemData `json:"result"`
+}
+
 // Main struct containing necessary data for items
 type GW2SpidyItemData struct {
-	DataID string `bson:"data_id" json:"data_id"`
-	Name   string `bson:"name" json:"name"`
-	Img    string `bson:"img,omitempty" json:"img"`
+	DataID                   int    `bson:"data_id" json:"data_id"`
+	Name                     string `bson:"name" json:"name"`
+	Img                      string `bson:"img,omitempty" json:"img"`
+	Rarity                   int    `bson:"rarity,omitempty" json:"rarity"`
+	RestrictionLevel         int    `bson:"restriction_level,omitempty" json:"restriction_level"`
+	TypeID                   int    `bson:"type_id,omitempty" json:"type_id"`
+	SubTypeID                int    `bson:"sub_type_id,omitempty" json:"sub_type_id"`
+	PriceLastChanged         string `bson:"price_last_changed,omitempty" json:"price_last_changed"`
+	MaxOfferUnitPrice        int    `bson:"max_offer_unit_price,omitempty" json:"max_offer_unit_price"`
+	MinSaleUnitPrice         int    `bson:"min_sale_unit_price,omitempty" json:"min_sale_unit_price"`
+	OfferAvailability        int    `bson:"offer_availability,omitempty" json:"offer_availability"`
+	SaleAvailability         int    `bson:"sale_availability,omitempty" json:"sale_availability"`
+	GW2DBExternalID          int    `bson:"gw2db_external_id,omitempty" json:"gw2db_external_id"`
+	SalePriceChangeLastHour  int    `bson:"sale_price_change_last_hour,omitempty" json:"sale_price_change_last_hour"`
+	OfferPriceChangeLastHour int    `bson:"offer_price_change_last_hour,omitempty" json:"offer_price_change_last_hour"`
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -77,34 +95,95 @@ func addSalvageHandler(response http.ResponseWriter, request *http.Request) {
 	// Pull out all the data in the database
 	var result []GW2SpidyItemData
 	err = collection.Find(nil).All(&result)
-	handleError(err, response, "Unable to execute template")
+	handleError(err, response, "Unable to retrieve data from collection")
 
 	// Execute the template with the data so we can show all the material items available
 	err = templates.ExecuteTemplate(response, "addSalvage.html", map[string]interface{}{"Materials": result})
 	handleError(err, response, "Unable to execute template")
 }
 
-// Handler to show page that allows the addition of new material types
-func addSalvateTypeHandler(response http.ResponseWriter, request *http.Request) {
-	// Retrieve items under the 'Crafting Material' category
-	requestURL := GW2SPIDY_URL + "all-items/5"
-	resp, err := http.Get(requestURL)
-	handleError(err, response, "Get request failed")
-	defer resp.Body.Close()
+// View all the item data
+func viewSalvageDataHandler(response http.ResponseWriter, request *http.Request) {
+	// Connect to the database
+	session, err := mgo.Dial(DB_ADDR)
+	handleError(err, response, "Unable to connect to database")
+	defer session.Close()
 
-	// Read contents of request
-	contents, err := ioutil.ReadAll(resp.Body)
-	handleError(err, response, "Unable to read body of response")
+	// Open up our collection
+	collection := session.DB(DB_NAME).C(COLLECTION_SALVAGE)
 
-	// Umarshal JSON into item data objects
-	itemList := GW2SpidyItemList{}
-	err = json.Unmarshal(contents, &itemList)
-	handleError(err, response, "Unable to Unmarshal contents of response")
+	// Pull out all the data in the database
+	var salvages []Salvage
+	err = collection.Find(nil).All(&salvages)
+	handleError(err, response, "Unable to retrieve salvage data from collection")
 
-	// TODO: Remove items already existing in salvageMaterials to avoid duplication
-	// Execute template to show checklist to add to database
-	err = templates.ExecuteTemplate(response, "addSalvageTypes.html", itemList.Items)
+	salvageItems := make([]GW2SpidyItemData, 0, 100)
+	materialItems := make([]GW2SpidyItemData, 0, 100)
+	requestURL := GW2SPIDY_URL + "item/"
+
+	for _, salvageItem := range salvages {
+		resp, err := http.Get(requestURL + salvageItem.ID)
+
+		if err != nil {
+			fmt.Println("Get request failed with id", salvageItem.ID)
+			continue
+		}
+
+		// Read contents of request
+		contents, err := ioutil.ReadAll(resp.Body)
+		handleError(err, response, "Unable to read body of response")
+
+		// Unmarshal JSON into item data
+		item := GW2SpidyItemResult{}
+		err = json.Unmarshal(contents, &item)
+		handleError(err, response, "Unable to Unmarshal contents of response")
+
+		salvageItems = append(salvageItems, item.Result)
+
+		// Retrieve all material data as needed
+		for _, materialItem := range salvageItem.Materials {
+			if isMaterialInSlice(materialItems, materialItem.ID) == false {
+				resp, err := http.Get(requestURL + materialItem.ID)
+
+				if err != nil {
+					fmt.Println("Get request failed with id", materialItem.ID)
+					continue
+				}
+
+				// Read contents of request
+				contents, err := ioutil.ReadAll(resp.Body)
+				handleError(err, response, "Unable to read body of response")
+
+				// Unmarshal JSON into item data
+				item := GW2SpidyItemResult{}
+				err = json.Unmarshal(contents, &item)
+				handleError(err, response, "Unable to Unmarshal contents of response")
+
+				materialItems = append(materialItems, item.Result)
+			}
+		}
+	}
+
+	// Execute the template with the data so we can show all the data available
+	err = templates.ExecuteTemplate(response, "viewSalvage.html", map[string]interface{}{"Materials": materialItems, "Items": salvageItems})
 	handleError(err, response, "Unable to execute template")
+}
+
+func isMaterialInSlice(materials []GW2SpidyItemData, id string) bool {
+	data_id, err := strconv.Atoi(id)
+
+	if err != nil {
+		fmt.Println("Unable to conver id", id, "to an int.")
+		return true
+	}
+
+	for _, mat := range materials {
+		if mat.DataID == data_id {
+			return true
+		}
+	}
+
+	return false
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -125,15 +204,13 @@ func libAddSalvageHandler(response http.ResponseWriter, request *http.Request) {
 
 	itemID := request.Form.Get("ID")
 	salvageCount, err := strconv.Atoi(request.Form.Get("SalvageCount"))
-	handleError(err, response, "Atoi failure")
+	handleError(err, response, "Atoi SalvageCount failure")
 
 	mat1 := request.Form.Get("material1")
-	mat1Count, err := strconv.Atoi(request.Form.Get("material1Count"))
-	handleError(err, response, "Atoi failure")
+	mat1Count, _ := strconv.Atoi(request.Form.Get("material1Count"))
 
 	mat2 := request.Form.Get("material2")
-	mat2Count, err := strconv.Atoi(request.Form.Get("material2Count"))
-	handleError(err, response, "Atoi failure")
+	mat2Count, _ := strconv.Atoi(request.Form.Get("material2Count"))
 
 	// Grab our collection
 	c := session.DB(DB_NAME).C(COLLECTION_SALVAGE)
@@ -199,52 +276,6 @@ func libAddSalvageHandler(response http.ResponseWriter, request *http.Request) {
 	http.Redirect(response, request, "../addSalvage", http.StatusFound)
 }
 
-/*
-// Handles the addition of new salvage material data
-func libAddSalvageTypeHandler(response http.ResponseWriter, request *http.Request) {
-	// Connect to the database
-	session, err := mgo.Dial(DB_ADDR)
-	handleError(err, response, "Unable to connect to database")
-	defer session.Close()
-
-	// Parse out the query parameters to make them available in the Form
-	request.ParseForm()
-
-	// Grab our collection
-	c := session.DB(DB_NAME).C(COLLECTION_SALVAGEMATS)
-	var itemData GW2SpidyItemData
-
-	// Loop through all query paramters and add them to the database
-	for id := range request.Form {
-		itemData.DataID = id
-		itemData.Name = request.Form.Get(id)
-
-		query := c.Find(bson.M{"data_id": id})
-		count, err := query.Count()
-
-		if err != nil {
-			fmt.Println("Unable to check if there are duplicates of id", id, err.Error())
-			continue
-		}
-
-		if count > 0 {
-			fmt.Println("Avoid duplicate entry of id", id)
-			continue
-		}
-
-		err = c.Insert(itemData)
-
-		if err != nil {
-			fmt.Println("Unable to add id ", id, "to salvageMaterials.", err.Error())
-		}
-	}
-
-	// Return to main page
-	// TODO: Return to a better page to show results/success
-	// TODO: Proper redirection
-	http.Redirect(response, request, "../addSalvage", http.StatusFound)
-}*/
-
 ///////////////////////////////////////////////////////////////////////////////
 //
 // Error Functions
@@ -269,8 +300,6 @@ func main() {
 	http.HandleFunc("/main", mainHandler)
 	http.HandleFunc("/addSalvage", addSalvageHandler)
 	http.HandleFunc("/lib/addSalvage", libAddSalvageHandler)
-
-	//http.HandleFunc("/addSalvageType", addSalvateTypeHandler)
-	//http.HandleFunc("/lib/addSalvageType", libAddSalvageTypeHandler)
+	http.HandleFunc("/viewSalvage", viewSalvageDataHandler)
 	http.ListenAndServe(":8080", nil)
 }
